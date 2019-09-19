a3be8577bda6b8d06f6fd4723ab7cbd03f0c4ca7
Hugo <ihfisk42@gmail.com>
2019-09-19 12:23:31 +0200

    Add gunfire

---
+++ Assets/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 50f;
+    
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Destroy(gameObject);
+    }
+}
